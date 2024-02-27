95e5ac585d6558c47d266d7dc1835c5a61fe86ac
Philip Donlon <pdonlon57@gmail.com>
2024-02-27 13:07:30 -0500

    Initial Commit

---
+++ HeartRate/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate
+//
+//  Created by Philip Donlon on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
